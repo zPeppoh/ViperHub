269d34c5cebce2355c5e514251ba2e0d6f77419b
GiuseppeVen99 <101936290+GiuseppeVen99@users.noreply.github.com>
2023-08-28 12:47:49 +0200

    Initial Commit

---
+++ src/main/java/it/viperacademy/viperhub/ViperHub.java
@@ -0,0 +1,17 @@
+package it.viperacademy.viperhub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ViperHub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
